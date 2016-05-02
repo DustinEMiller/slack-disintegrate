6bb5c73efd4824ba5f659f786359a55c971153ff
unknown <dustinmiller032@gmail.com>
2016-05-02 14:43:57 -0500

    Mongo creds

--- bot.js
+++ bot.js
@@ -20,10 +20,9 @@ var mongoOpts = {
   user: config.mongo.username,
   pass: config.mongo.password
 }
-'mongodb://username:password@host:port/database?options...'
 
 // Connect to MongoDB
-mongoose.connect('mongodb://'+config.mongo.username+':'+config.mongo.password+'@'+config.mongo.url+':'+config.mongo.port+'/'+config.mongo.dbName);
+mongoose.connect('mongodb://'+config.mongo.username+':'+config.mongo.password+'@'+config.mongo.host+':'+config.mongo.port+'/'+config.mongo.dbName);
 mongoose.connection.on('error', function(err) {
   console.error('MongoDB connection error: ' + err);
   process.exit(-1);
